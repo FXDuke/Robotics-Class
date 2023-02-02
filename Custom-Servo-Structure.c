e9be410b14f3ce51085c93254e03c9bc875490f1
Connor Fricke <113947184+FXDuke@users.noreply.github.com>
2023-02-02 14:09:17 -0500

    Task Queue
    added a system to queue tasks by toggling ServoInstance.Tasks to true, also added ClearTasks()

--- Custom-Servo-Structure.c
+++ Custom-Servo-Structure.c
@@ -8,8 +8,12 @@ typedef struct ServoObject {
     Servo Instance;
 
     bool Active = true;
+    bool Tasks = false; // Set to true for the next task to automatically be done 
 
     int Id = ServoAmount++;
+    
+    float TaskQueue[100][2];
+    int TaskQueueIndex = 0;
 
     float Rotation = 0.0;
     float TweenData[5] = {0,0.0,0.0,0.0,0.0}; // Active, Origin, Destination, Distance, Time
@@ -20,14 +24,22 @@ typedef struct ServoObject {
     void Begin(float newRotation, int Time) {
         int time = (Time) ? Time : 0;
         if (TweenData[0]==1) {
+            if (TaskQueueIndex<100 && Tasks==true) {
+                TaskQueue[TaskQueueIndex++] = {newRotation,time};
+            }
             return;
-        }
+        } 
         TweenData[0] = 1;
 
         TweenData[1] = Rotation;
         TweenData[2] = newRotation;
         TweenData[3] = fabs(newRotation-Rotation);
         TweenData[4] = time;
+    }
+    
+    void ClearTasks() {
+        TaskQueue = {};
+        TaskQueueIndex = 0;
     }
 
     void End() {
@@ -44,6 +56,15 @@ typedef struct ServoObject {
             if (TweenData[1]>TweenData[2] && Rotation<=TweenData[2] || TweenData[1]<TweenData[2] && Rotation>=TweenData[2]) {
                 TweenData[0] = 0;
                 Rotation = TweenData[2];
+                
+                if (TaskQueueIndex>=1 && Tasks==true) {
+                    Begin(TaskQueue[0][0],TaskQueue[0][1]);
+                    for (int i = 1; i < TaskQueueIndex; i++) {
+                        TaskQueue[i-1][0] = TaskQueue[i][0];
+                        TaskQueue[i-1][1] = TaskQueue[i][1];
+                    }
+                    TaskQueueIndex--;
+                }
             }
         }
         
