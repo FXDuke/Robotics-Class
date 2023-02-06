715472795903e7fc1cf446538e6f7788801dd67c
Connor Fricke <113947184+FXDuke@users.noreply.github.com>
2023-02-06 11:36:04 -0500

    First Rough Draft

---
+++ Linked-List-ServoManager.c
@@ -0,0 +1,59 @@
+// Work in progress attempt to make a linked list servo manager; rough draft
+
+typedef struct ServoNode {
+  struct ServoNode Next;
+  Servo Current;
+  int Index;
+} Node;
+
+
+typedef struct ServoTable {
+  struct ServoNode Head;
+  int Size;
+  void (*pairs)(void (*)(int i, struct ServNode v)); // Pairs
+  int (*ins)(Servo Object); // Insert
+  void (*del)(int Index); // Remove
+}
+
+struct ServoTable ServoManager;
+
+void Pairs(void (*ReturnFunction)(int i, struct ServoNode v)) {
+  for (int Index=0;Index<=ServoManager.Size;Index++) {
+    ServoManager.Head->Next;
+    ReturnFunction(Index,ServoManager.Head);
+  }
+}
+
+int Ins(Servo Object) {
+  struct ServoTable Last = ServoTable.Head;
+  ServoManager.Head = (Node *) malloc(sizeof(Node));
+  ServoManager.Head->Index = ServoTable.Size++;
+  ServoManager.Head->Current = Object;
+  ServoManager.Head->Next = Last;
+  return ServoManager.Size;
+}
+
+void Del(int Index) {
+  struct ServoNode Current = ServoManager.Head;
+  struct ServoNode Last;
+  while (Current->Next != NULL) {
+    if (Current.Index==Index) {
+      stuct ServoNode _Current = Current->Next;
+      for (int SizeIndex=Current.Index;SizeIndex<=ServoManager.Size;SizeIndex++) {
+        if (_Current!=NULL) {
+          _Current->Index = SizeIndex;
+        }
+      }
+      ServoManager.Size--;
+      Last->Next = Current->Next;
+      free(Current);
+      break;
+    }
+    Current = Current->Next;
+    Last = Current;
+  }
+}
+
+ServoManager.pairs = Pairs;
+ServoManager.ins = Ins;
+ServoManager.del = Del;
